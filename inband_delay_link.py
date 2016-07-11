7902ba4680f932e1a5426c32ecfb521ed0a6f246
hmd1st <hamed.nazari70@gmail.com>
2016-07-11 16:34:50 +0430

    Update inband_delay_link.py

--- inband_delay_link.py
+++ inband_delay_link.py
@@ -3,7 +3,7 @@
 #       s1-------s2--------s3--------s4
 #      /  \     /  \      /  \      /  \          
 #     h1  h2   h3  h4    h5  h6    h7   h8                   
-#             (c0)
+#             (c1)
 #
 
 from mininet.net import Mininet
